a572fa1edc397c7ced0000786e6c6aaa7d887e79
psynka <92454231+psynka@users.noreply.github.com>
2022-11-17 16:32:52 +0300

    Add files via upload

---
+++ database.py
@@ -0,0 +1,69 @@
+import OpenBlender
+import pandas as pd
+from sqlalchemy.ext.declarative import declarative_base
+import json
+from sqlalchemy import BigInteger, Column, Float, MetaData, Table, Integer, Date, String, Text, ForeignKey, create_engine
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, Boolean
+
+Base = declarative_base()
+meta = MetaData()
+engine = create_engine("sqlite:///database.db", echo=False)
+
+class Bitok(Base):
+    __tablename__ = 'Bitcoin_Data'
+    index = Column(BigInteger(), index=True)
+    volume = Column(Float(), primary_key=True)
+    market_cap = Column(Float())
+    high = Column(Float())
+    low = Column(Float())
+    close = Column(Float())
+    open = Column(Float())
+    timestamp = Column(Integer, ForeignKey('News_Data.timestamp'))
+
+
+class News(Base):
+    __tablename__ = 'News_Data'
+    index = Column(BigInteger(), index=True)
+    headline = Column(Text())
+    publisher = Column(Text())
+    title = Column(Text(), primary_key=True)
+    url = Column(Text())
+    timestamp = Column(Float(), primary_key=True)
+
+
+def pullObservationsToDF(parameters):
+    action = 'API_getObservationsFromDataset'
+    df = pd.read_json(json.dumps(OpenBlender.call(action, parameters)['sample']), convert_dates=False,
+                      convert_axes=False).sort_values('timestamp', ascending=False)
+    df.reset_index(drop=True, inplace=True)
+    return df
+
+def create_bitcoin(dataset):
+
+    dataset.to_sql('Bitcoin_Data', con=engine)
+
+def create_news(dataset):
+    dataset.to_sql('News_Data', con=engine)
+
+# цены биткоин
+parameters_prices = {
+    'token': '6375135895162959cf33dea6ncr1sn1ZuDno6ZjhRtnVK3nsznDuAv',
+    'id_dataset': '5d4c3b789516290b02fe3e24',
+}
+# новости
+parameters_news = {
+    'token': '6375135895162959cf33dea6ncr1sn1ZuDno6ZjhRtnVK3nsznDuAv',
+    'id_dataset': '5defce899516296bfe37c366',
+}
+
+df_bit = pullObservationsToDF(parameters_prices)
+#df_news = pullObservationsToDF(parameters_news)
+
+#create_news(df_news)
+create_bitcoin(df_bit)
+
+
+
+#Base.metadata.
+
+Base.metadata.create_all(engine)
